0bda81d0b18b96749bc083bb8d2f7d4c92f730de
hoachnt <hoachprogrammer@gmail.com>
2025-07-13 12:55:39 +0700

    Add id to github section

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 		<PageWrapper>
 			<Header name="Nguyen Tien Hoach" title="Developer" />
 
-			<GithubSection />
+			<GithubSection id="github" />
 
 			<AboutSection
 				id="about"
