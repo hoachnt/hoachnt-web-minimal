9139ec907d1a54a72bcf69cd5233e7daef4e7b39
hoachnt <hoachprogrammer@gmail.com>
2025-07-19 10:23:41 +0700

    Delete unused imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 import tailwindcss from "@tailwindcss/vite";
 
